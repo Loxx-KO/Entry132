c090de0e421f9e8fb147b63b66fb4f10a651e2b0
Loxx-KO <62263707+Loxx-KO@users.noreply.github.com>
2020-05-05 11:57:12 +0200

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -6,21 +6,29 @@ struct Enemy :
 {
 private:
 	//variables
+	std::string name;
+	int hp;
+	int exp;
+	int baseDmg;
+	int maxDmg;
 
 	//initialize functions
 	void initializeVariables();
 
 public:
 
-	Enemy(float x, float y, sf::Texture& texture);
+	Enemy(float x, float y, sf::Texture& texture, std::string name);
 	virtual ~Enemy();
-
 
 	//accessors
 	const sf::FloatRect CreateEnemyBox() const;
+	const int getEnemyHP() const;
+	const int getEnemyBaseDMG() const;
+	const int getEnemyMaxDMG() const;
+	const int getEnemyExp() const;
+	const std::string getName() const;
 
 	//functions
 	void update(const float& dtime);
 
 };
-
