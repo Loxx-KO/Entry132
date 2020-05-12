97c0d1b626b4877193224ce99799513f964b5672
Loxx-KO <62263707+Loxx-KO@users.noreply.github.com>
2020-05-12 12:11:09 +0200

    Update FightState.h

--- FightState.h
+++ FightState.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "pch.h"
 #include "States.h"
 #include "Button.h"
+#include "Bar.h"
 
 class FightState :
 	public States
@@ -13,32 +15,48 @@ private:
 
 	std::map<std::string, Button*> buttons;
 
-	//fighting stuff
+	//player
 	Player* player_fight;
+
+	int playerHpTemp;
 	int playerHp;
 	int playerDef;
 	int playerMana;
 	int playerDMG;
-	//int playerDmgTaken;
 	int playerExp;
 
-	//sf::RectangleShape playerbox;
-	//sf::Texture playerTexture;
-	//sf::Texture hpBarPlayer;
-	//sf::RectangleShape playerBar;
+	bool playerdead;
+	float playerBarWidth;
 	
+	//enemy
 	int enemyCount = 1;
-	//Enemy** enemies = new Enemy * [this->enemyCount];
+
 	Enemy** enemies;
+	std::vector<Enemy*> currentEnemies;
+
+	int enemyHpTemp;
 	int enemyHp;
 	int enemyDMG;
 	int enemyExp;
+	int runChance;
 
-	//sf::Texture hpBarEnemy;
-	//sf::RectangleShape enemyBar;
+	float enemyBarWidth;
 
-	bool dead;
+	//others
+	bool enemydead;
 	int gainEXP;
+	bool turn;
+
+	//bars
+	Bar* playerBar;
+
+	sf::Texture hpBarPlayer;
+	sf::RectangleShape PlayerBarHp;
+
+	Bar* enemyBar;
+
+	sf::Texture hpBarEnemy;
+	sf::RectangleShape enemyBarHp;
 
 	//functions
 	void initializeVariables();
@@ -48,16 +66,31 @@ private:
 	void initializeFonts();
 	void initializeKeybinds();
 	void initializeButtons();
+	void initializeBars();
 
 public:
 	FightState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<States*>* states, Enemy* enemy, std::string enemyName);
 	virtual ~FightState();
 
+	//accessors
+
 	//functions
+
+	void enemyAttack();
+	void attack();
+	void defend();
+	void useItem();
+	void tryRun();
+	void checkIfAnyoneDead();
+	
+	void changeBarSize();
 
 	void updateInput(const float& dtime);
 	void updateButtons();
 	void update(const float& dtime);
+
+	void sleepFor();
 	void renderButtons(sf::RenderTarget& target);
 	void render(sf::RenderTarget* target = nullptr);
 };
+
